cb3f63037410379a3206a645e4c3a6f6b34f0a5b
bujosa <davidbujosa@gmail.com>
2023-04-02 02:29:29 -0400

    feat: add lastPaidDate to the invoice model

--- contracts/facets/CaosFacet.sol
+++ contracts/facets/CaosFacet.sol
@@ -9,11 +9,14 @@ contract CaosFacet {
         uint salary;
         string position;
         uint totalHoursWorked;
+        address employeeAddress;
+        uint256 lastPaidDate;
     }
 
     mapping(address => Employee) private employees;
     mapping(address => uint) private payments;
     address[] private employeeAddresses;
+    mapping(string => uint) public rates;
 
     event EmployeeRegistered(
         string name,
@@ -23,7 +26,11 @@ contract CaosFacet {
         address indexed employeeAddress
     );
 
-    event PaymentProcessed(address indexed employeeAddress, uint256 amount);
+    event PaymentProcessed(
+        address indexed employeeAddress,
+        uint256 amount,
+        uint256 timestamp
+    );
 
     address public owner;
 
@@ -39,6 +46,14 @@ contract CaosFacet {
         _; // Continues execution if the modifier code above passed
     }
 
+    modifier isEmployee(address employeeAddress) {
+        require(
+            bytes(employees[employeeAddress].name).length != 0,
+            "Not a registered employee."
+        );
+        _;
+    }
+
     function registerEmployee(
         string memory name,
         string memory hireDate,
@@ -51,13 +66,19 @@ contract CaosFacet {
             employeeAddress != address(0),
             "Caos: employee address cannot be zero"
         );
+
+        require(rates[position] > 0, "Rate for position does not exist.");
+
         employees[employeeAddress] = Employee({
             name: name,
             hireDate: hireDate,
             salary: salary,
             position: position,
-            totalHoursWorked: totalHoursWorked
+            totalHoursWorked: totalHoursWorked,
+            employeeAddress: employeeAddress,
+            lastPaidDate: block.timestamp
         });
+
         emit EmployeeRegistered(
             name,
             hireDate,
@@ -67,33 +88,70 @@ contract CaosFacet {
         );
     }
 
-    function logHours(uint _hours) public {
+    function getEmployee(
+        address employeeAddress
+    )
+        public
+        view
+        isEmployee(employeeAddress)
+        returns (
+            string memory name,
+            string memory hireDate,
+            uint256 salary,
+            string memory position,
+            uint256 totalHoursWorked,
+            uint256 lastPaidDate
+        )
+    {
+        Employee storage employee = employees[employeeAddress];
+        name = employee.name;
+        hireDate = employee.hireDate;
+        salary = employee.salary;
+        position = employee.position;
+        totalHoursWorked = employee.totalHoursWorked;
+        lastPaidDate = employee.lastPaidDate;
+    }
+
+    // This function is used to log hours worked by an employee
+    function logHours(uint _hours) public isEmployee(msg.sender) {
         employees[msg.sender].totalHoursWorked += _hours;
     }
 
-    function calculatePayroll() public {
-        for (uint i = 0; i < employeeAddresses.length; i++) {
-            Employee storage employee = employees[employeeAddresses[i]];
-            uint pay = employee.totalHoursWorked * employee.salary;
-            payments[employeeAddresses[i]] = pay;
-            employee.totalHoursWorked = 0;
-        }
+    // This function is used to pay an employee
+    function processPayment(
+        address employeeAddress
+    ) public onlyOwner isEmployee(employeeAddress) {
+        uint256 paymentAmount = calculatePayment(employees[employeeAddress]);
+        require(paymentAmount > 0, "No hours worked.");
+        require(address(this).balance >= paymentAmount, "Insufficient funds.");
+        employees[employeeAddress].lastPaidDate = block.timestamp;
+        payable(employeeAddress).transfer(paymentAmount);
+        employees[employeeAddress].totalHoursWorked = 0;
+        payments[employeeAddress] += paymentAmount;
+        emit PaymentProcessed(employeeAddress, paymentAmount, block.timestamp);
     }
 
-    function processPayment() public payable {
-        require(payments[msg.sender] > 0, "No payment due.");
-        require(msg.value == payments[msg.sender], "Incorrect payment amount.");
-
-        // Transfer Ether to employee
-        (bool success, ) = payable(msg.sender).call{value: msg.value}("");
-        require(success, "Payment failed.");
-
-        payments[msg.sender] = 0;
-
-        emit PaymentProcessed(msg.sender, msg.value);
+    // get payment
+    function getPayment(
+        address employeeAddress
+    ) public view isEmployee(employeeAddress) returns (uint256) {
+        return payments[employeeAddress];
     }
 
     function logTransaction() public {
         // log payment transaction
+    }
+
+    function addRate(string memory position, uint rate) public onlyOwner {
+        rates[position] = rate;
+    }
+
+    function calculatePayment(
+        Employee memory employee
+    ) public view isEmployee(employee.employeeAddress) returns (uint256) {
+        uint256 hourlyRate = rates[employee.position];
+        uint256 totalPayment = hourlyRate * employee.totalHoursWorked;
+
+        return totalPayment;
     }
 }
